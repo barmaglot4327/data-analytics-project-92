2a7d9851b5842b79d03e6d2b7839ec3b1b78ec27
barmaglot4327 <petrov4327@mail.ru>
2025-06-23 22:39:57 +0300

    Add files via upload

---
+++ customers_count.sql
@@ -0,0 +1,6 @@
+select 
+COUNT(customer_id) as customers_count
+from customers;
+
+Запрос считает покупателей по колонке customer_id и создает таблицу с колонкой customers_count
+Инофрмацию берет из таблицы customers
